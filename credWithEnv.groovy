e15e178405ab4838e4f3e96b38c0dd8d0b0d123f
Tangang Atanga <geraldatanga@gmail.com>
2023-06-09 00:08:34 -0700

    Add credWithEnv.groovy agent modification

--- credWithEnv.groovy
+++ credWithEnv.groovy
@@ -1,16 +1,16 @@
 
-pipeline {
-    environment {
-        //env vars and credential retrivals
-        SOME_VAR = 'SOME VALUE'
-    }
-
-    agent none
-
-    stages {
-        stage('Foo') {
-            steps {
-                sh(script: 'echo "SOME_VAR is $SOME_VAR"')
+podTemplate(containers: [
+     containerTemplate(name: 'ubuntu', image: 'ubuntu:latest', command: 'sleep', args: '99d')
+]) {
+    node(POD_LABEL) {
+        environment {
+            SOME_VAR = 'SOME VALUE'
+        }
+        stages {
+            stage('Foo') {
+                steps {
+                    sh(script: 'echo "SOME_VAR is $SOME_VAR"')
+                }
             }
         }
     }
