52957cfecb8d7be2d1ddcc0c06bfc50affdeb100
Tangang Atanga <geraldatanga@gmail.com>
2023-06-09 01:08:29 -0700

    Add credWithEnv.groovy agent modification

--- credWithEnv.groovy
+++ credWithEnv.groovy
@@ -3,16 +3,14 @@ podTemplate(containers: [
      containerTemplate(name: 'ubuntu', image: 'ubuntu:latest', command: 'sleep', args: '99d')
 ]) {
     node(POD_LABEL) {
-        environment {
-            SOME_VAR = 'SOME VALUE'
-        }
-
         stage('Foo Ubuntu') {
             container('ubuntu') {
                 stage('check env vars') {
                     sh 'cd ~'
                     sh 'ls -la'
-                    sh "echo $SOME_VAR"
+                    withEnv(["SOME_VAR=SOME VAR"]) {
+                        sh "echo $SOME_VAR"
+                    }
                 }
             }
         }
